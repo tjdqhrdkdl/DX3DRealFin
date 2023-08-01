26ddd41696581ee4c70be56b9d18788f5e8042bc
kss2256 <kkaw2357@gmail.com>
2023-08-01 15:56:11 +0900

    Monster_Start

--- Engine_SOURCE/MonsterBase.h
+++ Engine_SOURCE/MonsterBase.h
@@ -1,5 +1,9 @@
 #pragma once
 #include "yaGameObject.h"
+#include "yaTime.h"
+#include "yaState.h"
+#include "yaTransform.h"
+#include "yaRigidbody.h"
 
 namespace ya
 {
@@ -20,11 +24,35 @@ namespace ya
         virtual void Render() = 0;
 
 
+    public:
+
+        GameObject* GetPlayerObject() { return mPlayerObject ; }
+
+        Vec3 GetPlayerPos() { return mPlayerPos; }
+        Vec3 GetMonster2PlayerNormalize() { return mMonster2PlayerNormalize; }
+        Vec3 GetPlayer2MonsterNormalize() { return mPlayer2MonsterNormalize; }
+        bool IsPlayerFront() { return mbPlayerFront; }
+      
+
+        void SetPlayerObject(GameObject* target) { mPlayerObject = target; }
+
+        void SetPlayerPos(Vec3 pos) { mPlayerPos = pos; }
+		void SetMonster2PlayerNormalize(Vec3 mormal) { mMonster2PlayerNormalize = mormal; }
+		void SetPlayer2MonsterNormalize(Vec3 mormal) { mPlayer2MonsterNormalize = mormal; }
+        void SetPlayerFront(bool front) {  mbPlayerFront = front; }
+
 
     private:
 
+        GameObject*         mPlayerObject;
+        State*              mMonsterState;
+
+        Vec3                mPlayerPos;
+        Vec3                mMonster2PlayerNormalize;
+        Vec3                mPlayer2MonsterNormalize;
 
 
+        bool                mbPlayerFront;
 
     };
 }
--- Engine_SOURCE/yaSpearman.cpp
+++ Engine_SOURCE/yaSpearman.cpp
@@ -1,1 +1,44 @@
 #include "yaSpearman.h"
+
+
+namespace ya
+{
+	void Spearman::Initalize()
+	{
+
+
+		//몬스터 할일 있나
+		MonsterBase::Initalize();
+	}
+
+
+	void Spearman::FixedUpdate()
+	{
+		Transform* tr = GetComponent<Transform>();
+		Vec3 rot = tr->GetRotation();
+		
+		Vec3 playerPos = GetPlayerPos();
+		Vec3 monsterPos = GetComponent<Transform>()->GetPosition();
+		
+
+
+		Quaternion quater = Quaternion::FromToRotation(tr->Forward(), playerPos);
+		Vec3 monDirection = quater.ToEuler();
+		
+
+		tr->SetRotation(Vec3(0.0f, rot.y + monDirection.y, 0.0f));
+
+
+
+		GameObject::FixedUpdate();
+	}
+
+	void Spearman::Render()
+	{
+
+
+
+		GameObject::Render();
+	}
+
+}
