0c1bb288edca36d9c2f14baa12f707e034f14a1a
성복 강 <bok5454@naver.com>
2023-08-02 16:49:21 +0900

    카메라 장애물에 딱붙는 문제 수정

--- Engine_SOURCE/yaCameraScript.cpp
+++ Engine_SOURCE/yaCameraScript.cpp
@@ -115,8 +115,8 @@ namespace ya
 					mChildPos *= mDistFromTarget;
 
 					//y축 이동 한계 지정
-					if (mChildPos.y < -mDistFromTarget + mDistFromTarget / 1.5)
-						mChildPos.y = -mDistFromTarget + mDistFromTarget / 1.5;
+					if (mChildPos.y < -mDistFromTarget + mDistFromTarget / 1.2)
+						mChildPos.y = -mDistFromTarget + mDistFromTarget / 1.2;
 					if (mChildPos.y > mDistFromTarget - mDistFromTarget / 5)
 						mChildPos.y = mDistFromTarget - mDistFromTarget / 5;
 
@@ -163,7 +163,7 @@ namespace ya
 		if (hit.isHit)
 		{
 			int a = 0;
-			tr->SetPosition(hit.contact);
+			tr->SetPosition(hit.contact - direction);
 		}
 	}
 }
