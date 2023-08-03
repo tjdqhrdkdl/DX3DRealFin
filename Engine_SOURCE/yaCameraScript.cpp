8e6f0c8c8749c75028ff2d0b48c4edd914568318
성복 강 <bok5454@naver.com>
2023-08-03 15:43:18 +0900

    인코딩 해결하려고 파일 인코딩 저장형식을 바꿨는데 파일이 다 날아가버림

---
+++ Engine_SOURCE/yaCameraScript.cpp
@@ -0,0 +1,169 @@
+#include "yaCameraScript.h"
+#include "yaTransform.h"
+#include "yaGameObject.h"
+#include "yaInput.h"
+#include "yaTime.h"
+#include "yaCollisionManager.h"
+
+extern ya::Application application;
+
+namespace ya
+{
+	CameraScript::CameraScript()
+		: Script()
+		, mChildPos(Vector3(0, 0, -40))
+		, mThetaAxisY(1.57)
+		, mThetaAxisX(1.57)
+		, mDistFromTarget(40)
+		, mDelayTime(0.1f)
+		, mDelayTimeChecker(0)
+		, mbFirstInit(false)
+		, mbMouseMove(true)
+	{
+	}
+
+	CameraScript::~CameraScript()
+	{
+	}
+
+	void CameraScript::Initialize()
+	{
+		Transform* tr = GetOwner()->GetComponent<Transform>();
+
+	}
+
+	void CameraScript::Update()
+	{
+		Transform* tr = GetOwner()->GetComponent<Transform>();
+
+
+		Vector3 pos = tr->GetPosition();
+
+
+		tr->SetPosition(pos);
+		if (mTarget != nullptr)
+		{
+			TrackTarget();
+			MouseMove();
+			ObstacleDetection();
+		}
+	}
+	void CameraScript::Render()
+	{
+
+	}
+	void CameraScript::TrackTarget()
+	{
+
+		Vector3 targetPos = mTarget->GetComponent<Transform>()->GetPosition();
+		mQueDelayedTargetPos.push(targetPos);
+		Transform* tr = GetOwner()->GetComponent<Transform>();
+
+
+		if (mDelayTimeChecker < mDelayTime && mbFirstInit)
+			mDelayTimeChecker += Time::DeltaTime();
+		else
+		{
+			Vector3 delayedPos = mQueDelayedTargetPos.front();
+			mQueDelayedTargetPos.pop();
+			mDelayedTargetPos = delayedPos;
+			tr->SetPosition(delayedPos + mChildPos);
+		}
+
+	}
+	void CameraScript::MouseMove()
+	{
+		if (Input::GetKeyDown(eKeyCode::ESC))
+		{
+			mbMouseMove = !mbMouseMove;
+		}
+		if (mbMouseMove)
+		{
+			//최초의 마우스 위치에 카메라가 영향받지 않도록.
+			if (!mbFirstInit)
+			{
+				SetCursorPos(application.GetWidth() / 2, application.GetHeight() / 2);
+				mbFirstInit = true;
+			}
+
+
+			// 마우스의 이동 거리(픽셀) 측정 - 반영 - 마우스 위치 고정 
+			else
+			{
+				Vector2 mousePos = Input::GetMousePosition();
+				POINT center = { application.GetWidth() / 2, application.GetHeight() / 2 };
+				ScreenToClient(application.GetHwnd(), &center);
+
+				Vector2 mouseMovement = { mousePos.x - center.x, center.y - mousePos.y };
+				Transform* tr = GetOwner()->GetComponent<Transform>();
+
+				//디버깅시에 문제생기는 부분 막음.
+				if (Time::DeltaTime() < 0.1f)
+				{
+					//두번 계산해줄 것이다.
+					//카메라를 원점(플레이어) 기준으로 먼저 위치를 이동시키고
+					//카메라 오브젝트의 회전을 바꿔준다.
+
+					//구 이동
+					mChildPos += 60 * tr->Right() * mouseMovement.x * Time::DeltaTime();;
+					mChildPos.Normalize();
+					mChildPos *= mDistFromTarget;
+
+
+					mChildPos += 60 * tr->Up() * mouseMovement.y * Time::DeltaTime();
+					mChildPos.Normalize();
+					mChildPos *= mDistFromTarget;
+
+					//y축 이동 한계 지정
+					if (mChildPos.y < -mDistFromTarget + mDistFromTarget / 10)
+						mChildPos.y = -mDistFromTarget + mDistFromTarget / 10;
+					if (mChildPos.y > mDistFromTarget - mDistFromTarget / 10)
+						mChildPos.y = mDistFromTarget - mDistFromTarget / 10;
+
+
+					//회전
+					Vector3 pos = tr->GetPosition();
+					Vector3 UpVector = Vector3(0.0, 1.0, 0.0);
+
+					Vector3 targetPos = mDelayedTargetPos;
+					Vector3 newForward = targetPos - pos;
+					newForward.Normalize();
+
+					Vector3 forward = newForward;
+
+					Vector3 right = UpVector.Cross(forward);
+					right.Normalize();
+
+					Vector3 up = forward.Cross(right);
+					up.Normalize();
+
+					tr->IsCamera(true);
+					tr->SetForward(forward);
+					tr->SetUp(up);
+					tr->SetRight(right);
+
+
+
+
+				}
+
+
+				SetCursorPos(application.GetWidth() / 2, application.GetHeight() / 2);
+			}
+		}
+	}
+	void CameraScript::ObstacleDetection()
+	{
+		Transform* tr = GetOwner()->GetComponent<Transform>();
+		Vector3 direction = tr->GetPosition() - mDelayedTargetPos;
+		direction.Normalize();
+		std::vector<eLayerType> layers = {};
+		layers.push_back(eLayerType::Ground);
+		RayHit hit = CollisionManager::RayCast(mTarget, direction, layers);
+		if (hit.isHit)
+		{
+			int a = 0;
+			tr->SetPosition(hit.contact);
+		}
+	}
+}
---
+++ Engine_SOURCE/yaPlayerScript.cpp
@@ -0,0 +1,118 @@
+#include "yaPlayerScript.h"
+#include "yaPlayer.h"
+#include "yaTransform.h"
+#include "yaGameObject.h"
+#include "yaInput.h"
+#include "yaTime.h"
+#include "yaAnimator.h"
+#include "yaObject.h";
+#include "yaMeshRenderer.h"
+#include "yaResources.h"
+#include "yaRigidbody.h"
+#include "yaActionScript.h"
+#include "yaGrappleHookScript.h"
+
+namespace ya
+{
+	PlayerScript::PlayerScript()
+		: Script()
+	{
+	}
+
+	PlayerScript::~PlayerScript()
+	{
+	}
+
+	void PlayerScript::Initialize()
+	{
+		Transform* tr = GetOwner()->GetComponent<Transform>();
+		{
+			// 플레이어의 forward를 구분하기위한 object
+			// 후에 mesh 씌우면 없앨 예정
+			GameObject* face = object::Instantiate<GameObject>(eLayerType::Player, tr);
+			face->SetName(L"face");
+			Transform* faceTr = face->GetComponent<Transform>();
+			faceTr->SetPosition(Vector3(0.0f, 0.5f, 0.5f));
+			faceTr->SetScale(Vector3(0.4f, 0.4f, 0.4f));
+			MeshRenderer* faceRenderer = face->AddComponent<MeshRenderer>();
+			faceRenderer->SetMaterial(Resources::Find<Material>(L"BasicMaterial"));
+			faceRenderer->SetMesh(Resources::Find<Mesh>(L"CubeMesh"));
+		}
+	}
+
+	void PlayerScript::Update()
+	{
+		ActionScript* action = GetOwner()->GetScript<ActionScript>();
+		action->SetSpeed(120.0f); // �Ŀ� �÷��̾� status�� ����
+
+		Transform* tr = GetOwner()->GetComponent<Transform>();
+		float speed = 120.0f; // 후에 플레이어 status로 변경
+
+		if (Input::GetKey(eKeyCode::L))
+		{
+			action->Move(tr->Right());
+		}
+		if (Input::GetKey(eKeyCode::J))
+		{
+			action->Move(-tr->Right());
+		}
+		if (Input::GetKey(eKeyCode::I))
+		{
+			action->Move(tr->Forward());
+		}
+		if (Input::GetKey(eKeyCode::K))
+		{
+			action->Move(-tr->Forward());
+		}
+
+		if (Input::GetKey(eKeyCode::SPACE))
+		{
+			action->Jump();
+		}
+
+		if (Input::GetKey(eKeyCode::O))
+		{
+			action->Rotate(tr->Up());
+		}
+		if (Input::GetKey(eKeyCode::U))
+		{
+			action->Rotate(-tr->Up());
+		}
+
+		if (Input::GetKey(eKeyCode::F))
+		{
+			GrappleHookScript* grap = GetOwner()->GetScript<GrappleHookScript>();
+			grap->GrappleHook();
+		}
+	}
+
+
+	void PlayerScript::Render()
+	{
+	}
+
+	void PlayerScript::OnCollisionEnter(Collider2D* collider)
+	{
+	}
+
+	void PlayerScript::OnCollisionStay(Collider2D* collider)
+	{
+	}
+
+	void PlayerScript::OnCollisionExit(Collider2D* collider)
+	{
+	}
+
+	void PlayerScript::Start()
+	{
+	}
+
+	void PlayerScript::Action()
+	{
+	}
+
+	void PlayerScript::End()
+	{
+	}
+
+}
