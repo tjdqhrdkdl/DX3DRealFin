03a36755db1d42953f46a4bdae5c4d085cdf1945
성복 강 <bok5454@naver.com>
2023-08-25 16:30:07 +0900

    Bone Animator Event 구조 추가 AnimationData MeshData에 통합

--- Engine_SOURCE/yaMeshData.h
+++ Engine_SOURCE/yaMeshData.h
@@ -14,6 +14,8 @@ namespace ya
 		~MeshData();
 
 		static MeshData* LoadFromFbx(const std::wstring& path);
+		
+		void LoadAnimationFromFbx(const std::wstring& path, const std::wstring& name);
 
 		virtual void Save(const std::wstring& path);
 		virtual HRESULT Load(const std::wstring& path) { return S_OK; }
@@ -21,20 +23,30 @@ namespace ya
 
 		MeshObject* Instantiate(eLayerType type);
 
+
+		void Play(const std::wstring animName);
+
+		std::function<void()>& GetAnimationStartEvent(const std::wstring& name);				
+		std::function<void()>& GetAnimationCompleteEvent(const std::wstring& name);				
+		std::function<void()>& GetAnimationEndEvent(const std::wstring& name);					
+		std::function<void()>& GetAnimationFrameEvent(const std::wstring& name, UINT index);	
+
+
+		std::vector<graphics::StructedBuffer*> GetBoneFrameData() { return mBoneFrameDataVector; }
+		graphics::StructedBuffer* GetBoneOffset() { return mBoneOffset; }
+
+	private:
 		std::vector<BoneMatrix>* GetBones() { return &mBones; }
 		std::vector<BoneAnimationClip> GetAnimClips() { return mAnimClip; }
 		void PushBackAnimClip(BoneAnimationClip& clip) { mAnimClip.push_back(clip); }
 
 		void PushBackBoneFrameData(graphics::StructedBuffer* buffer) { mBoneFrameDataVector.push_back(buffer); }
-		std::vector<graphics::StructedBuffer*> GetBoneFrameData() { return mBoneFrameDataVector; }
 
 		void SetBoneOffset(graphics::StructedBuffer* buffer) { mBoneOffset = buffer; }
-		graphics::StructedBuffer* GetBoneOffset() { return mBoneOffset; }
+
 
 		void SetAnimationClipCount(UINT num) { mAnimationClipCount = num; }
 		UINT GetAnimationClipCount() { return mAnimationClipCount; }
-
-		void Play(const std::wstring animName);
 	private:
 		std::vector<std::shared_ptr<Mesh>>  mMeshes;
 		std::vector<std::vector<std::shared_ptr<Material>>> mMaterialsVec;
@@ -51,5 +63,7 @@ namespace ya
 		graphics::StructedBuffer* mBoneOffset; // 각 뼈의 offset 행렬 () 각뼈의 위치를 TPOSE로 되돌리는 행렬
 
 		UINT mAnimationClipCount;
+
+		class BoneAnimator* mRepresentBoneAnimator;
 	};
 }
