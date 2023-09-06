ef079cb5755a6eb793552bb8f0cccaf565c36dd4
jueh <jueh11@naver.com>
2023-09-06 15:31:02 +0900

    1. player mesh 추가 - animation 등록, 추가중.. 2. AnimationLoad 에서 새로운 파일을 부르면 덮어 씌워지던 로직을 누적되게 하는 로직으로 변경 (yaMeshData.cpp)

---
+++ Engine_SOURCE/yaPlayerProjectileScript.cpp
@@ -0,0 +1,41 @@
+#include "yaPlayerProjectileScript.h"
+
+
+namespace ya
+{
+	PlayerProjectileScript::PlayerProjectileScript()
+		: Script()
+	{
+	}
+
+	PlayerProjectileScript::~PlayerProjectileScript()
+	{
+	}
+
+	void PlayerProjectileScript::Initialize()
+	{
+	}
+
+	void PlayerProjectileScript::Update()
+	{
+	}
+
+	void PlayerProjectileScript::Render()
+	{
+	}
+
+	void PlayerProjectileScript::OnCollisionEnter(Collider2D* collider)
+	{
+		int a = 0;
+
+		//몬스터 공격
+	}
+
+	void PlayerProjectileScript::OnCollisionStay(Collider2D* collider)
+	{
+	}
+
+	void PlayerProjectileScript::OnCollisionExit(Collider2D* collider)
+	{
+	}
+}
