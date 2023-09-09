d27c69c08c5effe501014603720248f4ead5f1a7
HWNOTE <ekdrn1@naver.com>
2023-09-09 16:59:55 +0900

    imgui 데모 끔

--- 44Engine/guiEditor.cpp
+++ 44Engine/guiEditor.cpp
@@ -250,7 +250,7 @@ namespace gui
 
 	void Editor::ImGui_Run()
 	{
-		bool show_demo_window = true;
+		bool show_demo_window = false;
 		bool show_another_window = false;
 		ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
