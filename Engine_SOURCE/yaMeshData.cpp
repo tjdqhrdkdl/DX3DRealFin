cba354ed5faf0caa930f31c6c9b5887bbfb452eb
성복 강 <bok5454@naver.com>
2023-10-26 11:07:38 +0900

    애니메이션 데이터 리소스화

--- Engine_SOURCE/yaMeshData.cpp
+++ Engine_SOURCE/yaMeshData.cpp
@@ -12,7 +12,7 @@
 #include "yaBoundarySphere.h"
 #include "yaScene.h"
 #include "StrConverter.h"
-
+#include "yaAnimationData.h"
 namespace ya
 {
 	MeshData::MeshData()
@@ -20,7 +20,6 @@ namespace ya
 		, mAnimationClipCount(0)
 		, mIFrameCount(0)
 		, mBoneOffset(nullptr)
-		, mAnimationOffset(Vector3::Zero)
 		, mbBoundarySphere(false)
 	{
 	}
@@ -584,143 +583,43 @@ namespace ya
 
 	HRESULT MeshData::AnimationLoad(const std::wstring& path, FILE* file, bool bLast)
 	{
-		std::string strPath = StrConverter::ConvertUnicodeToUTF8(path);
+		if (mAnimationData == nullptr)
+			mAnimationData = std::make_shared<AnimationData>();
+		if (bLast)
+		{		//Find 애니메이션 데이터
+			std::string strPath = StrConverter::ConvertUnicodeToUTF8(path);
 
-		std::filesystem::path CurparentPath = std::fs::absolute(gResPath);
+			std::filesystem::path CurparentPath = std::fs::absolute(gResPath);
 
-		std::filesystem::path parentPath = strPath;
-		parentPath = parentPath.parent_path().parent_path();
-		parentPath += L"\\AnimationData\\";
+			std::filesystem::path parentPath = strPath;
+			parentPath = parentPath.parent_path().parent_path();
+			parentPath += L"\\AnimationData\\";
 
-		CurparentPath /= parentPath;
+			CurparentPath /= parentPath;
 
-		std::wstring name = std::filesystem::path(path).stem();
-		name += L".animationdata";
+			std::wstring name = std::filesystem::path(path).stem();
+			name += L".animationdata";
 
-		CurparentPath += name;
+			CurparentPath += name;
 
-		std::wstring fullPath = CurparentPath;
+			std::wstring fullPath = CurparentPath;
+			std::wstring key = fullPath;
+			std::shared_ptr<AnimationData> resource = Resources::Find<AnimationData>(key);
 
-		file = nullptr;
-		_wfopen_s(&file, fullPath.c_str(), L"rb");
-		if (file == nullptr)
-			return S_FALSE;
-
-		//본 사이즈 
-		UINT boneSize = 0;
-		fread(&boneSize, sizeof(UINT), 1, file);	
-
-
-		//애니 클립 사이즈 저장
-		UINT AnimClipSize = 0;
-		fread(&AnimClipSize, sizeof(UINT), 1, file);
-		mAnimationClipCount += AnimClipSize;
-
-		std::vector<BoneAnimationClip> animClip;
-
-		animClip.resize(AnimClipSize);
-		for (size_t i = 0; i < AnimClipSize; i++)
-		{
-			LoadWString(animClip[i].name, file);
-
-			fread(&animClip[i].startTime, sizeof(double), 1, file);
-			fread(&animClip[i].endTime, sizeof(double), 1, file);
-			fread(&animClip[i].timeLength, sizeof(double), 1, file);
-			fread(&animClip[i].mode, sizeof(int), 1, file);
-			fread(&animClip[i].updateTime, sizeof(float), 1, file);
-			fread(&animClip[i].startFrame, sizeof(int), 1, file);
-			fread(&animClip[i].endFrame, sizeof(int), 1, file);
-			fread(&animClip[i].frameLength, sizeof(int), 1, file);			
-		}
-
-		// 본정보들 전부 저장	
-		//UINT iFrameCount = 0;
-		mBones.resize(boneSize);
-		for (size_t i = 0; i < boneSize; i++)
-		{
-			LoadWString(mBones[i].name, file);
-			fread(&mBones[i].depth, sizeof(int), 1, file);
-			fread(&mBones[i].parentIdx, sizeof(int), 1, file);
-			fread(&mBones[i].bone, sizeof(Matrix), 1, file);
-			fread(&mBones[i].offset, sizeof(Matrix), 1, file);
-
-			std::vector<std::vector<BoneKeyFrame>> keyFrames;
-			keyFrames.resize(AnimClipSize);
-			//mBones[i].keyFrames.resize(AnimClipSize);
-			for (size_t j = 0; j < AnimClipSize; j++)
-			{				
-				UINT boneKeyFramesSize;
-				fread(&boneKeyFramesSize, sizeof(UINT), 1, file);
-				
-				keyFrames[j].resize(boneKeyFramesSize);
-
-				for (UINT k = 0; k < keyFrames[j].size(); k++)
-				{
-					fread(&keyFrames[j][k].time, sizeof(double), 1, file);
-					fread(&keyFrames[j][k].frame, sizeof(int), 1, file);
-					fread(&keyFrames[j][k].translate, sizeof(Vector3), 1, file);
-					fread(&keyFrames[j][k].scale, sizeof(Vector3), 1, file);
-					fread(&keyFrames[j][k].rotation, sizeof(Vector4), 1, file);
-				}
-				mIFrameCount = max(mIFrameCount, (UINT)keyFrames[j].size());
-
-				/*
-				mBones[i].keyFrames[j].resize(boneKeyFramesSize);
-				for (UINT k = 0; k < mBones[i].keyFrames[j].size(); k++)
-				{
-					fread(&mBones[i].keyFrames[j][k].time, sizeof(double), 1, file);
-					fread(&mBones[i].keyFrames[j][k].frame, sizeof(int), 1, file);
-					fread(&mBones[i].keyFrames[j][k].translate, sizeof(Vector3), 1, file);
-					fread(&mBones[i].keyFrames[j][k].scale, sizeof(Vector3), 1, file);
-					fread(&mBones[i].keyFrames[j][k].rotation, sizeof(Vector4), 1, file);
-				}
-				iFrameCount = max(iFrameCount, (UINT)mBones[i].keyFrames[j].size());	
-				*/		
-			}			
-			mBones[i].keyFrames.insert(mBones[i].keyFrames.end(), keyFrames.begin(), keyFrames.end());
-		}
-
-		fclose(file);
-
-		mAnimClip.insert(mAnimClip.end(), animClip.begin(), animClip.end());
-
-		if(bLast)
-		{
-			std::vector<std::vector<BoneFrameTransform>> vecFrameTrans;
-			vecFrameTrans.resize(mAnimationClipCount);
-
-			for (size_t i = 0; i < mBones.size(); ++i)
+			if (resource != nullptr)
 			{
-				for (size_t k = 0; k < mAnimationClipCount; k++)
-				{
-					vecFrameTrans[k].resize((UINT)mBones.size() * mIFrameCount);
-					for (size_t j = 0; j < mBones[i].keyFrames[k].size(); ++j)
-					{
-						vecFrameTrans[k][(UINT)mBones.size() * j + i]
-							= BoneFrameTransform
-						{
-							Vector4(mBones[i].keyFrames[k][j].translate.x + mAnimationOffset.x
-								, mBones[i].keyFrames[k][j].translate.y + mAnimationOffset.y
-								, mBones[i].keyFrames[k][j].translate.z + mAnimationOffset.z , 0.f)
-							, Vector4(mBones[i].keyFrames[k][j].scale.x
-								, mBones[i].keyFrames[k][j].scale.y
-								, mBones[i].keyFrames[k][j].scale.z, 0.f)
-							, mBones[i].keyFrames[k][j].rotation
-						};
-					}
-				}
+				mAnimationData = resource;
 			}
-
-			for (size_t i = 0; i < mAnimationClipCount; i++)
+			else
 			{
-				graphics::StructedBuffer* boneFrameData = new graphics::StructedBuffer();
-				boneFrameData->Create(sizeof(BoneFrameTransform), (UINT)mBones.size() * mIFrameCount
-					, eSRVType::SRV, vecFrameTrans[i].data(), false);
-				PushBackBoneFrameData(boneFrameData);
-			}	
+				mAnimationData->LoadFin(path);
+			}
 		}
-		
+		else
+			mAnimationData->LoadNotFin(path);
+
 		return S_OK;
+
 	}
 
 
@@ -760,15 +659,15 @@ namespace ya
 			meshObject->PushBackObject(gameObj);
 			mChildObjects.push_back(gameObj);
 
-			if (mAnimationClipCount > 0)
+			if (mAnimationData != nullptr)
 			{
 				BoneAnimator* animator = gameObj->AddComponent<BoneAnimator>();
 
 				if (i == 0)
 				{
 					mRepresentBoneAnimator = animator;		
-					animator->SetBones(&mBones);
-					animator->SetAnimaitionClip(&mAnimClip);
+					animator->SetBones(GetBones());
+					animator->SetAnimaitionClip(GetAnimClips());
 				}
 				else
 					animator->SetParentAnimator(mRepresentBoneAnimator);
@@ -813,6 +712,26 @@ namespace ya
 	std::function<void()>& MeshData::GetAnimationFrameEvent(const std::wstring& name, UINT index)
 	{
 		return mRepresentBoneAnimator->GetFrameEvent(name, index);
+	}
+
+	std::vector<BoneMatrix>* MeshData::GetBones()
+	{
+		return mAnimationData->GetBones();
+	}
+
+	std::vector<BoneAnimationClip>* MeshData::GetAnimClips()
+	{
+		return mAnimationData->GetAnimClips();
+	}
+
+	UINT MeshData::GetAnimationClipCount()
+	{
+		return mAnimationData->GetAnimationClipCount();
+	}
+
+	std::vector<graphics::StructedBuffer*> MeshData::GetBoneFrameData()
+	{
+		return mAnimationData->GetBoneFrameDataVector();
 	}
 
 
--- Engine_SOURCE/yaMeshData.h
+++ Engine_SOURCE/yaMeshData.h
@@ -8,6 +8,7 @@
 namespace ya
 {
 	class Scene;
+	class AnimationData;
 	class MeshData : public Resource
 	{
 	public:
@@ -39,25 +40,23 @@ namespace ya
 		std::function<void()>& GetAnimationFrameEvent(const std::wstring& name, UINT index);	
 
 
-		std::vector<graphics::StructedBuffer*> GetBoneFrameData() { return mBoneFrameDataVector; }
 		graphics::StructedBuffer* GetBoneOffset() { return mBoneOffset; }
 
 		//Save --------- public
-		std::vector<BoneMatrix>* GetBones() { return &mBones; }
-		std::vector<BoneAnimationClip> GetAnimClips() { return mAnimClip; }
+		std::vector<BoneMatrix>* GetBones();
+		std::vector<BoneAnimationClip>* GetAnimClips();
+		UINT GetAnimationClipCount();
+		std::vector<graphics::StructedBuffer*> GetBoneFrameData();
+
+		void PushBackAnimClip(BoneAnimationClip& clip) { mAnimClip.push_back(clip); }
+		void SetAnimationClipCount(UINT num) { mAnimationClipCount = num; }
+		void PushBackBoneFrameData(graphics::StructedBuffer* buffer) { mBoneFrameDataVector.push_back(buffer); }
+
 
 		std::vector<std::shared_ptr<Mesh>> GetMeshs() { return mMeshes; }
 
 		std::vector<std::vector<std::shared_ptr<Material>>> GetMaterialsVec() { return mMaterialsVec; }
-
-		void PushBackAnimClip(BoneAnimationClip& clip) { mAnimClip.push_back(clip); }
-
-		void PushBackBoneFrameData(graphics::StructedBuffer* buffer) { mBoneFrameDataVector.push_back(buffer); }
-
 		void SetBoneOffset(graphics::StructedBuffer* buffer) { mBoneOffset = buffer; }
-
-		void SetAnimationClipCount(UINT num) { mAnimationClipCount = num; }
-		UINT GetAnimationClipCount() { return mAnimationClipCount; }
 		class BoneAnimator* GetAnimator() { return mRepresentBoneAnimator; }
 		MeshObject* GetMeshObject() { return mMeshObject; }
 		std::vector<GameObject*> GetChildObjects() { return mChildObjects; }
@@ -66,8 +65,6 @@ namespace ya
 
 		void SaveWString(const std::wstring& _str, FILE* _pFile);
 		void LoadWString(std::wstring& _str, FILE* _pFile);
-
-		void SetAnimationOffset(Vector3 offset) { mAnimationOffset = offset; }
 		
 		
 		void SetBoundarySphere(bool b) { mbBoundarySphere = b; }
@@ -80,26 +77,26 @@ namespace ya
 		std::vector<GameObject*> mChildObjects;
 
 		std::wstring mFullPath;
+		graphics::StructedBuffer* mBoneOffset; // 각 뼈의 offset 행렬 () 각뼈의 위치를 TPOSE로 되돌리는 행렬
 
 		//3D Animation 정보
 		std::vector<BoneAnimationClip> mAnimClip;
 		std::vector<BoneMatrix> mBones;
-	
 		std::vector<graphics::StructedBuffer*> mBoneFrameDataVector; // 전체 본 프레임 정보 ( 크기, 이름, 회전) 프레임 갯수만큼
-		graphics::StructedBuffer* mBoneOffset; // 각 뼈의 offset 행렬 () 각뼈의 위치를 TPOSE로 되돌리는 행렬
-
 		UINT mAnimationClipCount;
 		UINT mIFrameCount;
 
+
+
 		Vector3 mMeshCenter;
 		float mBoundarySphereRadius;
-		
 		class BoneAnimator* mRepresentBoneAnimator;
 		MeshObject* mMeshObject;
 
-		Vector3 mAnimationOffset;
 		//Frustum Culling 여부
 		bool mbBoundarySphere;
+
+		std::shared_ptr<AnimationData> mAnimationData;
 
 	};
 }
